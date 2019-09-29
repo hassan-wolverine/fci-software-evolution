689c340df5578a9dd81dd22746e2ee6ca543b39b
hassan-wolverine <hassanwolverin@gmail.com>
2019-09-29 10:36:25 +0200

    added the "Add" method

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -8,10 +8,16 @@ namespace ConsoleApplication1
 {
     class Program
     {
+        static int add(int x, int y)
+        {
+            return x + y;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world");
-            Console.WriteLine("hello hasan");
+            Console.WriteLine("hello world!");
+
+            var z = add(2, 3);
         }
     }
 }
