59e3f8b9c172b3eab8dcd3069883ca54173e8f35
hassan-wolverine <hassanwolverin@gmail.com>
2019-10-13 10:42:36 +0200

    Update Program.cs

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -13,7 +13,7 @@ namespace ConsoleApplication1
             return x - y;
         }
         
-        static int add(int x, int y)
+        static int add_(int x, int y)
         {
             return x + y;
         }
